17cc5ea10142844ec517d01fda490a480bd08c44
Saxon Payne <saxattack101@gmail.com>
2024-10-04 08:09:53 -0700

    finnished assignment #1 ✅

--- MortgageCalculator/hm_mortgage.cpp
+++ MortgageCalculator/hm_mortgage.cpp
@@ -6,26 +6,29 @@ using std::endl;
 
 double mortPrin(float home_price, float down_payment) {
   float actual = (home_price - down_payment);
+  cout << "Principal Loan Amount: " << actual << endl;
   return actual;
 }
 
 double propTax(float tax_rate, float home_price) {
-  float monRate = (tax_rate * home_price) / 12;
+  float monRate = ((tax_rate / 100) * home_price) / 12;
+  cout << "Monthly Property Tax: " << monRate << endl;
   return monRate;
 }
 
 double monInsur(float insur_rate, float home_price) {
-  float mon_insur = (insur_rate * home_price) / 12;
+  float mon_insur = ((insur_rate / 100) * home_price) / 12;
+  cout << "Monthly Insurance: " << mon_insur << endl;
   return mon_insur;
 }
 
 int main() {
   double homePrice;
   double downPayment;
-  double lengthOfLoan;
-  double monthlyIntRate;
-  double propertyTaxRate;
-  double insuranceRate;
+  int lengthOfLoan;       // in months
+  double monthlyIntRate;  // entered as decimal
+  double propertyTaxRate; // entered as decimal
+  double insuranceRate;   // entered as decimal
 
   std::cout << "Please provide Home Price: ";
   std::cin >> homePrice;
@@ -33,32 +36,32 @@ int main() {
   std::cout << "Please provide your Down Payment: ";
   std::cin >> downPayment;
 
-  std::cout << "Please provide your Loan Duration: ";
+  std::cout << "Please provide your Loan Duration (in months): ";
   std::cin >> lengthOfLoan;
 
-  std::cout << "Please provide your Monthly interest Rate: ";
+  std::cout << "Please provide your Monthly Interest Rate (as decimal): ";
   std::cin >> monthlyIntRate;
 
-  std::cout << "Please provide your Property Tax Rate: ";
+  std::cout << "Please provide your Property Tax Rate (as decimal): ";
   std::cin >> propertyTaxRate;
 
-  std::cout << "Please provide your Insurance Rate: ";
+  std::cout << "Please provide your Insurance Rate (as decimal): ";
   std::cin >> insuranceRate;
 
   double p = mortPrin(homePrice, downPayment);
-  double i = monthlyIntRate;
-  float n = lengthOfLoan;
-  float t = propTax(propertyTaxRate, homePrice);
-  float r = monInsur(insuranceRate, homePrice);
+  double i = (monthlyIntRate / 100);
+  int n = lengthOfLoan;
+  double t = propTax(propertyTaxRate, homePrice);
+  double r = monInsur(insuranceRate, homePrice);
 
-  cout << p << endl;
-  cout << i << endl;
-  cout << n << endl;
-  cout << t << endl;
-  cout << r << endl;
+  // Mortgage Calculation using the formula
 
-  double monthlyMortgage = (p * i * pow((1 + i), n)) / (pow((1 + i), n) - 1);
+  double top = (p * i * (pow((1 + i), n)));
+  double bottom = pow((1 + i), n) - 1;
+  double monthlyMortgage = (top / bottom) + t + r;
 
-  std::cout << "Your estimated monthly payment: " << monthlyMortgage << endl;
+  std::cout << "Your estimated monthly payment: $" << monthlyMortgage << endl;
+  cout << "Your total paid is: $" << monthlyMortgage * n << endl;
+  cout << "Your addiontal payment is: $" << (monthlyMortgage * n) - p << endl;
   return 0;
 }
